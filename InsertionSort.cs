e2c703b7773ca6926d258a6b9a7636ab28481163
bhumika1111 <bhoyarpatil@434>
2022-08-15 20:31:35 +0530

    *Print The Sorted List*

---
+++ InsertionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithamProgram
+{
+    /// <summary>
+    ///   Here We Are Checking Insertion Sorting.
+    /// </summary>
+    class InsertionSort
+    {
+        public void CheckSorting()
+        {
+            int[] arr = { 23, 9, 85, 12 };
+
+            int n = 4, i, j, val, flag;
+
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                        flag = 1;
+                }
+            }
+            Console.WriteLine("Sorted Array :");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+        }
+    }
+}

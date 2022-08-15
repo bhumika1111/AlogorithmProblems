3eeaed5af0afa2465036fd9f2b8b7b2b750c7c7a
bhumika1111 <bhoyarpatil@434>
2022-08-15 20:33:56 +0530

    *Print The Sorted List*

---
+++ BubbleSorting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithamProgram
+{
+    /// <summary>
+    /// Here We Are Checking BubbleSorting .
+    /// </summary>
+    public class BubbleSorting
+    {
+         public void Sorting()
+            {
+                int[] arr = { 78, 55, 45, 98, 13 };
+                int temp;
+
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    for (int i = 0; i <= arr.Length - 2; i++)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            temp = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = temp;
+                        }
+                    }
+                }
+                Console.WriteLine("Sorted : ");
+                foreach (int p in arr)
+                    Console.Write(p + " ");
+                Console.ReadKey();
+            
+        }
+    }
+}
